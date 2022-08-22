cdb8881f35e54c6aa77e8ed599c80ca8bdcc3c82
EtienneUrbano <urbano.etienne@laposte.net>
2022-08-22 10:45:01 +0200

    first commit - img + pictureComponent

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Dimupi
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.3 by Etienne URBANO.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PictureComponent } from './picture/picture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PictureComponent
   ],
   imports: [
     BrowserModule
