aaaa0633d51aaab6561e1a30b90e2e2375fb97b9
EtienneUrbano <urbano.etienne@laposte.net>
2022-08-22 15:45:07 +0200

    Like/unlike on picture + modele picture

---
+++ src/app/models/picture.model.ts
@@ -0,0 +1,8 @@
+export class picture {
+  constructor(public title: string,
+              public description: string,
+              public creationDate: Date,
+              public imageUrl: string,
+              public likes: number) {
+  }
+}
--- src/app/picture/picture.component.ts
+++ src/app/picture/picture.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-picture',
   templateUrl: './picture.component.html',
   styleUrls: ['./picture.component.scss']
 })
-export class PictureComponent implements OnInit {
+export class PictureComponent implements OnInit, OnDestroy {
+  // J'utilise le bang '!' pour spécifier à l'IDE que je compte initialiser la
+  // valeur de l'attribut juste après (dans le constructeur)
+  title!: string;
+  description!: string;
+  creationDate!: Date;
+  likes!: number;
+  imageUrl!: string;
+  textLikeButton!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.title = 'Leaves';
+    this.description = 'Feuilles';
+    this.creationDate = new Date();
+    this.imageUrl = "https://cdn.pixabay.com/photo/2017/08/08/01/17/colorful-2609978_960_720.jpg";
+    this.likes = 4;
+    this.textLikeButton = 'I like it !';
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  onClickLike() {
+    if (this.textLikeButton === 'I like it !') {
+      this.likes++;
+      this.textLikeButton = 'Oops, unlike';
+    } else {
+      this.likes--;
+      this.textLikeButton = 'I like it !';
+    }
   }
 
 }
