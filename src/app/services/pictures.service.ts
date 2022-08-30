7c45d255eb612b5421aada41d8cd2ea42ce00ea8
EtienneUrbano <urbano.etienne@laposte.net>
2022-08-30 17:45:19 +0200

    Ajout footer avec flex content

--- src/app/services/pictures.service.ts
+++ src/app/services/pictures.service.ts
@@ -9,16 +9,16 @@ export class PicturesService {
     {
       id: 1,
       title: 'leaves',
-      description: 'feuilles multicolores',
+      description: 'colourful tree leaves',
       creationDate: new Date(),
       imageUrl: "https://cdn.pixabay.com/photo/2017/08/08/01/17/colorful-2609978_960_720.jpg",
       likes: 75,
-      location: 'dans la forêt'
+      location: 'in the forest'
     },
     {
       id: 2,
       title: 'milky way',
-      description: 'Voie lactée',
+      description: 'Our milky way in the universe, seen from the Earth',
       creationDate: new Date(),
       imageUrl: "https://cdn.pixabay.com/photo/2020/06/13/17/50/milky-way-5295153_960_720.jpg",
       likes: 8,
@@ -27,20 +27,20 @@ export class PicturesService {
     {
       id: 3,
       title: 'fruit tray',
-      description: 'Plateau de fruits frais et colorés',
+      description: 'tray of colorful and fresh fruits, next to the swimming pool',
       creationDate: new Date(),
       imageUrl: "https://cdn.pixabay.com/photo/2019/07/29/05/53/pool-4369976_960_720.jpg",
       likes: 14,
-      location: 'Au bord de l\'eau'
+      location: 'At the edge of the water'
     },
     {
       id: 4,
       title: 'Countryside',
-      description: 'Châlets et lac en campagne',
+      description: 'mountain cabins right by the quiet lake',
       creationDate: new Date(),
       imageUrl: "https://cdn.pixabay.com/photo/2019/09/03/13/13/countryside-4449408_960_720.jpg",
       likes: 22,
-      location: 'en pleine montagne',
+      location: 'in the middle of the mountains',
       price: 584.2
     }
   ];
