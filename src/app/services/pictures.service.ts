87245f76fa6c3f27aaaa1760aba92ac94f42619b
EtienneUrbano <urbano.etienne@laposte.net>
2022-08-31 14:58:10 +0200

    Intégration liste du Json file + amélioration des banières picture-list

--- src/app/services/pictures.service.ts
+++ src/app/services/pictures.service.ts
@@ -1,98 +1,78 @@
 import { Injectable } from '@angular/core';
 import { Picture } from "../models/picture.model";
-import picturesData from '../../assets/pictures-items.json';
+import picturesJsonData from '../../assets/pictures-items.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PicturesService {
-  pictures: Picture[] = [
-    {
-      id: 1,
-      title: "leaves",
-      description: "colourful tree leaves",
-      creationDate: new Date("2020-04-30T02:15:12.356Z"),
-      imageUrl: "https://cdn.pixabay.com/photo/2017/08/08/01/17/colorful-2609978_960_720.jpg",
-      likes: 75,
-      liked: false,
-      location: "in the forest"
-    },
-    {
-      id: 2,
-      title: "milky way",
-      description: "Our milky way in the universe, seen from the Earth",
-      creationDate: new Date("2020-04-30T02:15:12.356Z"),
-      imageUrl: "https://cdn.pixabay.com/photo/2020/06/13/17/50/milky-way-5295153_960_720.jpg",
-      likes: 8,
-      liked: true,
-      price: 4516.57
-    },
-    {
-      id: 3,
-      title: "fruit tray",
-      description: "tray of colorful and fresh fruits, next to the swimming pool",
-      creationDate: new Date("2022-04-17T06:30:19 -02:00"),
-      imageUrl: "https://cdn.pixabay.com/photo/2019/07/29/05/53/pool-4369976_960_720.jpg",
-      likes: 14,
-      liked: false,
-      location: "At the edge of the water"
-    },
-    {
-      id: 4,
-      title: "Countryside",
-      description: "mountain cabins right by the quiet lake",
-      creationDate: new Date("2022-10-25T01:22:13.356Z"),
-      imageUrl: "https://cdn.pixabay.com/photo/2019/09/03/13/13/countryside-4449408_960_720.jpg",
-      likes: 22,
-      liked: false,
-      location: "in the middle of the mountains",
-      price: 584.2
-    },
-    {
-      id: 5,
-      title: "Mosque",
-      description: "Mosque architecture",
-      creationDate: new Date("2022-05-15T01:22:13.356Z"),
-      imageUrl: "https://cdn.pixabay.com/photo/2021/11/04/05/33/dome-6767422_960_720.jpg",
-      likes: 29,
-      liked: true,
-      location: "Abu Dhabi"
-    },
-    {
-      id: 6,
-      title: "Flower",
-      description: "Pink flower of Frangipani/Plumeroa",
-      creationDate: new Date("2021-11-09T02:21:04.356Z"),
-      imageUrl: "https://cdn.pixabay.com/photo/2022/08/27/08/21/plumeria-7413942_960_720.jpg",
-      likes: 8,
-      liked: false,
-      location: "unknown"
-    }
-  ]
+  // ------------ Récupération des Pictures du JSON File ------------
+  pictures: Picture[] = picturesJsonData
+
+  // ----------------- Ancienne base locale en dur ------------------
+  // pictures: Picture[] = [
   //   {
   //     id: 1,
-  //     title: 'leaves',
-  //     description: 'colourful tree leaves',
-  //     creationDate: new Date(),
+  //     title: "leaves",
+  //     description: "colourful tree leaves",
+  //     creationDate: new Date("2020-04-30T02:15:12.356Z"),
   //     imageUrl: "https://cdn.pixabay.com/photo/2017/08/08/01/17/colorful-2609978_960_720.jpg",
   //     likes: 75,
   //     liked: false,
-  //     location: 'in the forest'
+  //     location: "in the forest"
   //   },
   //   {
   //     id: 2,
-  //     title: 'milky way',
-  //     description: 'Our milky way in the universe, seen from the Earth',
-  //     creationDate: new Date(),
+  //     title: "milky way",
+  //     description: "Our milky way in the universe, seen from the Earth",
+  //     creationDate: new Date("2020-04-30T02:15:12.356Z"),
   //     imageUrl: "https://cdn.pixabay.com/photo/2020/06/13/17/50/milky-way-5295153_960_720.jpg",
   //     likes: 8,
   //     liked: true,
   //     price: 4516.57
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "fruit tray",
+  //     description: "tray of colorful and fresh fruits, next to the swimming pool",
+  //     creationDate: new Date("2022-04-17T06:30:19 -02:00"),
+  //     imageUrl: "https://cdn.pixabay.com/photo/2019/07/29/05/53/pool-4369976_960_720.jpg",
+  //     likes: 14,
+  //     liked: false,
+  //     location: "At the edge of the water"
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Countryside",
+  //     description: "mountain cabins right by the quiet lake",
+  //     creationDate: new Date("2022-10-25T01:22:13.356Z"),
+  //     imageUrl: "https://cdn.pixabay.com/photo/2019/09/03/13/13/countryside-4449408_960_720.jpg",
+  //     likes: 22,
+  //     liked: false,
+  //     location: "in the middle of the mountains",
+  //     price: 584.2
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "Mosque",
+  //     description: "Mosque architecture",
+  //     creationDate: new Date("2022-05-15T01:22:13.356Z"),
+  //     imageUrl: "https://cdn.pixabay.com/photo/2021/11/04/05/33/dome-6767422_960_720.jpg",
+  //     likes: 29,
+  //     liked: true,
+  //     location: "Abu Dhabi"
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "Flower",
+  //     description: "Pink flower of Frangipani/Plumeroa",
+  //     creationDate: new Date("2021-11-09T02:21:04.356Z"),
+  //     imageUrl: "https://cdn.pixabay.com/photo/2022/08/27/08/21/plumeria-7413942_960_720.jpg",
+  //     likes: 8,
+  //     liked: false,
+  //     location: "unknown"
   //   }
-  // ];
-
-  // ------------ Tentative de récupération des Pictures du JSON File ------------
-  // pictures: any = (picturesData as any).default;
+  // ]
 
   getAllPictures(): Picture[] {
     return this.pictures;
